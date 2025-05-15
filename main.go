432391122f48175fdfa5baf216087a823a7c1bc0
yuanjinghao <805595058@qq.com>
2025-05-15 21:40:34 +0800

    Third commit

--- main.go
+++ main.go
@@ -2,29 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	fmt.Print("Hello Git!")
-}
-
-func maxMin(nums ...int) (int, int) {
-	if len(nums) == 0 {
-		panic("必须至少包含一个数字！")
-	}
-
-	max := math.MinInt
-	min := math.MaxInt
-
-	for _, num := range nums {
-		if max < num {
-			max = num
-		}
-		if min > num {
-			min = num
-		}
-	}
-
-	return max, min
 }
