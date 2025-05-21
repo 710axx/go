2dc6a1f8cbded4783c70f8ec55770f3e395f5974
yuanjinghao <805595058@qq.com>
2025-05-21 15:55:59 +0800

    Second commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is my Go test!
+
+Gotcha!
--- main.go
+++ main.go
@@ -2,35 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	max, min := maxMin(1, 3, 2, 5, 7, 9)
-	fmt.Print(max, min)
-}
+	fmt.Print("Hello Git!")
 
-func add(a, b int) (sum int) {
-	sum = a + b
-	return
-}
-
-func maxMin(nums ...int) (int, int) {
-	if len(nums) == 0 {
-		panic("必须至少包含一个数字！")
-	}
-
-	max := math.MinInt
-	min := math.MaxInt
-
-	for _, num := range nums {
-		if max < num {
-			max = num
-		}
-		if min > num {
-			min = num
-		}
-	}
-
-	return max, min
+	fmt.Print("Rebase test...")
 }
